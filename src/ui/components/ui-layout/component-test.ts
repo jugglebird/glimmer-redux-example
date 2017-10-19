ac46632beea49bc67436cfe82dfa7aa21db034e1
toran billups <toranb@gmail.com>
2017-10-19 07:16:26 -0500

    TESTS: updated the integration test

--- src/ui/components/ui-layout/component-test.ts
+++ src/ui/components/ui-layout/component-test.ts
@@ -8,6 +8,19 @@ module('Component: glimmer-redux-example', function(hooks) {
 
   test('it renders', async function(assert) {
     await this.render(hbs`<ui-layout />`);
-    assert.equal(this.containerElement.textContent, 'Glimmer Redux! 2');
+    let text = this.containerElement.querySelector('.glimmer-text').textContent;
+    let number = this.containerElement.querySelector('.number').textContent;
+    assert.equal(text, 'Glimmer Redux 2');
+    assert.equal(number, 'number 2');
+
+    this.containerElement.querySelector('.glimmer-update').click();
+    let done = assert.async();
+    setTimeout(() => {
+      text = this.containerElement.querySelector('.glimmer-text').textContent;
+      number = this.containerElement.querySelector('.number').textContent;
+      assert.equal(text, 'Glimmer Redux 5');
+      assert.equal(number, 'number 5');
+      done();
+    }, 10);
   });
 });
